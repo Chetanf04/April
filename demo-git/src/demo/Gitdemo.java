e9d3fd0c39a197190cb5a6a7daf34b96b3d1fa65
Chetanf04 <fegadechetan45@gmail.com>
2025-05-05 22:30:12 +0530

    First Commit

---
+++ demo-git/src/demo/Gitdemo.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Gitdemo {
+    public static void main(String[] args) {
+    	System.out.println("Git Demo");
+    	for(int i = 1; i <= 2; i++) {
+    		
+			int j = 2;
+			while(j <= 3) {
+				int k = 1;
+				do {
+					System.out.println("i: " + i + " j " + j + " k " + k);
+					k++;
+				} while(k <= 2);
+				j++;
+			}
+		}
+    	}
+}
